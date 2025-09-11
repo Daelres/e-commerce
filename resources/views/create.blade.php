c56b183ff988a0d643783db91b9b32fd0e070987
Daelres <daelres10@gmail.com>
2025-09-11 11:57:48 -0500

    views básicas

---
+++ resources/views/create.blade.php
@@ -0,0 +1,23 @@
+<html>
+<head>
+    <title>Document</title>
+</head>
+<body>
+    <h1>Listado productos</h1>
+    <form action="/posts" method="POST">
+
+        <label for="name">Nombre:</label>
+        <input type="text" id="name" name="name" required>
+        <br>
+        <label for="description">Descripción:</label>
+        <input type="text" id="description" name="description" required>
+        <br>
+        <label for="price">Precio:</label>
+        <input type="text" id="price" name="price" required>
+        <br>
+        <label for="image">Imagen:</label>
+        <input type="image" id="image" name="image" required>
+        <br>
+        <label for="brand">Marca:</label>
+        <input type="text" id="brand" name="brand" required>
+</html>
