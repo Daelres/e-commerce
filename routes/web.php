5d67665744e221df1a4209127b886a6f46511612
Daelres <daelres10@gmail.com>
2025-09-11 10:55:42 -0500

    Rutas básico

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,15 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route :: get('products', function () {
+    return "Listado de productos";
+});
+
+Route :: get('products/create', function () {
+    return "Formulario de creación de productos";
+});
+
+Route :: get('products/{id}/{category?}', function ($idProducto, $category = "General") {
+    return "Detalle de cada producto {$idProducto} de la categoría {$category}";
+});
